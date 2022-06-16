78d3611dcfdd88e78221d20308d14a59fe61b754
Chanmi0205 <chanmi1155@naver.com>
2022-06-16 23:49:03 +0900

    portfolio_220616
    맞춘 문제 : https://www.acmicpc.net/problem/1000
    (두 수 0보다 큰 A와 ,10보다 작은 B를 입력받아 더한 값을 출력하는 알고리즘 문제)

---
+++ src/portfolio_220616/Main.java
@@ -0,0 +1,21 @@
+package portfolio_220616;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		// 두 수 A, B 입력받기
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		
+		if ( 0 < A && B < 10 ) {
+			System.out.println(A+B);
+		} else {
+			System.out.println("조건에 맞지 않은 입력값입니다.");
+		}
+	}
+
+}
