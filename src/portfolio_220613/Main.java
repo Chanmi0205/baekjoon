e6b599716dd57b4e5699b425efa889624a844a30
Chanmi0205 <chanmi1155@naver.com>
2022-06-21 22:20:32 +0900

    portfolio_220613
    맞춘 문제 : https://www.acmicpc.net/problem/2438
    (밑변과 높이가 N인 입력값을 받아 직각삼각형 출력하는 알고리즘 문제)

---
+++ src/portfolio_220613/Main.java
@@ -0,0 +1,19 @@
+package portfolio_220613;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+
+            for(int i=0; i<N; i++) {
+                for(int j=0; j<=i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+    }
+
+}
