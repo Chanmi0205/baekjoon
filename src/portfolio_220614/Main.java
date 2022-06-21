c271ec656e9fd40b4f22a08e0e6c30b01676c433
Chanmi0205 <chanmi1155@naver.com>
2022-06-21 22:23:36 +0900

    portfolio_220614
    맞춘 문제 : https://www.acmicpc.net/problem/10171
    (귀여운 고양이 출력하는 알고리즘 문제)

---
+++ src/portfolio_220614/Main.java
@@ -0,0 +1,17 @@
+package portfolio_220614;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // 2557 문제
+        System.out.println("Hello World!");
+
+        // 10171 문제
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+    }
+
+}
