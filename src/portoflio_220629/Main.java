c14101c27c71e52e9088b5a35cc768be402c28c4
Chanmi0205 <chanmi1155@naver.com>
2022-06-29 20:03:09 +0900

    portfolio_220629
    푼 문제 : https://www.acmicpc.net/problem/2741
    (1부터 N까지 출력하는 알고리즘 문제)

---
+++ src/portoflio_220629/Main.java
@@ -0,0 +1,17 @@
+package portoflio_220629;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int N = scan.nextInt();
+		
+		if ( 1<= N && N <= 100000 )
+			for(int i=1; i<=N; i++) 
+				System.out.println(i);
+	}
+
+}
