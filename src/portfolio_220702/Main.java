ecedf2f855233e8e352bfaf397d298820e528fb9
Chanmi0205 <chanmi1155@naver.com>
2022-07-02 16:57:13 +0900

    portfolio_220702
    푼 문제 : https://www.acmicpc.net/problem/10950
    (두 정수 A, B를 입력받은 다음, A+B를 출력하는 알고리즘 문제)

---
+++ src/portfolio_220702/Main.java
@@ -0,0 +1,32 @@
+package portfolio_220702;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int T = scan.nextInt();
+		int Case[][] = new int[T][2];
+		int sum[] = new int[T];
+		
+		for(int i=0; i<Case.length; i++) {
+			for(int j=0; j<Case[i].length; j++) {
+				Case[i][j] = scan.nextInt();
+			}
+		}
+		
+		for(int i=0; i<Case.length; i++) {
+			for(int j=0; j<Case[i].length; j++) {
+				sum[i] += Case[i][j];
+			}
+		}
+		
+		for(int i=0; i<sum.length; i++) {
+			System.out.println(sum[i]);
+		}
+		
+	}
+
+}
