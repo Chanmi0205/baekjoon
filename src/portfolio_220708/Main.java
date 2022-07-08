457c726b1d486ad5e80937ba98e108249245e10c
Chanmi0205 <chanmi1155@naver.com>
2022-07-08 23:23:11 +0900

    portfolio_220708
    푼 문제 : https://www.acmicpc.net/problem/2558
    (A+B를 출력하는 알고리즘 문제)

---
+++ src/portfolio_220708/Main.java
@@ -0,0 +1,16 @@
+package portfolio_220708;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        
+        System.out.println(A+B);
+		
+	}
+
+}
