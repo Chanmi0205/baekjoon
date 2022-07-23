dd3ac9f60d05cdff93dfe2e46ad947a31fd86497
Chanmi0205 <chanmi1155@naver.com>
2022-07-23 23:35:54 +0900

    portfolio_220723
    맞춘 문제 : https://www.acmicpc.net/problem/2588
    (num1, num2가 주어졌을 때 num1과 num2의 각각 자리수 곱한 값을 출력한 후 마지막으로 num1*num2를 출력하는
    알고리즘 문제)

---
+++ src/portfolio_220723/Main.java
@@ -0,0 +1,25 @@
+package portfolio_220723;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		int num1 = scan.nextInt();
+		int num2 = scan.nextInt();
+		int test = num2;
+		int num[] = new int[3];
+		
+		for(int i=0; i<num.length; i++) {
+			num[i] = test%10;
+			test /= 10;
+			System.out.println(num1 * num[i]);
+		}
+		
+		System.out.println(num1 * num2);
+		
+		
+	}
+
+}
